cfa2f8f748296a485ab5568b418ac8cb87b556df
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2021-12-19 18:03:52 +0545

    UDW SI calculator.

--- Flutter Projects/widgets/lib/main.dart
+++ Flutter Projects/widgets/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Form Example",
-      home: FormExample(),
+      home: SICalculator(),
     );
   }
 }
--- Flutter Projects/widgets/lib/screen/form.dart
+++ Flutter Projects/widgets/lib/screen/form.dart
@@ -190,3 +190,256 @@ class _FormExampleState extends State<FormExample> {
 AppBar myAppBar(BuildContext context) {
   return AppBar(title: Text("My form example."));
 }
+
+// Classwork
+class SICalculator extends StatefulWidget {
+  const SICalculator({Key? key}) : super(key: key);
+
+  @override
+  _SICalculatorState createState() => _SICalculatorState();
+}
+
+class _SICalculatorState extends State<SICalculator> {
+  final fristCtrl = TextEditingController();
+  final secondCtrl = TextEditingController();
+  final thirdCtrl = TextEditingController();
+  final _fromKey = GlobalKey<FormState>();
+  String result = "";
+
+  interest<num>(p, r, t) {
+    num si = (p * t * r) / 100;
+    return si;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: myAppBar(context),
+      body: Form(
+        key: _fromKey,
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  "SI Calculator",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: "Pacifico-Regular",
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "field is Empty";
+                    }
+                    return null;
+                  },
+                  controller: fristCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: "FuzzyBubbles-Bold",
+                  ),
+                  decoration: InputDecoration(
+                    labelText: "Principle",
+                    hintText: "Enter the principle....",
+                    suffixIcon: Icon(Icons.clear),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "field is Empty";
+                    }
+                    return null;
+                  },
+                  controller: secondCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: "FuzzyBubbles-Bold",
+                  ),
+                  decoration: InputDecoration(
+                    labelText: "Rate",
+                    hintText: "Enter the rate....",
+                    suffixIcon: Icon(Icons.clear),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "field is Empty";
+                    }
+                    return null;
+                  },
+                  controller: thirdCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: "FuzzyBubbles-Bold",
+                  ),
+                  decoration: InputDecoration(
+                    labelText: "Time",
+                    hintText: "Enter the time....",
+                    suffixIcon: Icon(Icons.clear),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.purple,
+                        minimumSize: Size(40.0, 30.0),
+                      ),
+                      onPressed: () {
+                        if (_fromKey.currentState!.validate()) {
+                          setState(() {
+                            result = (interest(
+                                    int.parse(fristCtrl.text),
+                                    int.parse(secondCtrl.text),
+                                    int.parse(thirdCtrl.text)))
+                                .toString();
+                          });
+                        }
+                      },
+                      child: Text(
+                        "Claculate",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "FuzzyBubbles-Bold",
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                        minimumSize: Size(40.0, 30.0),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          fristCtrl.text = "";
+                          secondCtrl.text = "";
+                          thirdCtrl.text = "";
+                        });
+                      },
+                      child: Text(
+                        "Clear Form",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "FuzzyBubbles-Bold",
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.redAccent,
+                        minimumSize: Size(40.0, 30.0),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          result = "";
+                        });
+                      },
+                      child: Text(
+                        "Clear Result",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "FuzzyBubbles-Bold",
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        width: 130,
+                        height: 80,
+                        child: Center(
+                          child: Text(
+                            "SI",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontFamily: "Pacifico-Regular",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        width: 130,
+                        height: 80,
+                        child: Center(
+                          child: Text(
+                            result,
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.black,
+                              fontFamily: "Pacifico-Regular",
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
