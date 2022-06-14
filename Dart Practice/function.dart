176c3bdcb1c1edf190e18f3f76aa13dd35152c8d
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2022-06-14 13:42:37 +0545

    stream builder example

--- Dart Practice/function.dart
+++ Dart Practice/function.dart
@@ -2,23 +2,27 @@ void main() {
   var c = sum(2, 3);
   print("The sum is $c.");
 
-  printCities('ktm', 'pokhara', city3:'morang', city4: "jhapa");
+  printCities('ktm', 'pokhara', city3: 'morang', city4: "jhapa");
 
   // Anonymous function
   var l = [10, 20, 30, 40];
   l.forEach((element) {
     print('square: ${element * element}');
   });
+
+  print(DateTime.now().hour.runtimeType);
 }
 
-//Collable function 
+//Collable function
 int sum(int a, int b) {
   return (a + b);
 }
+
 // Another way
 int sum1(int a, int b) => (a + b);
 
-void printCities (String city1, String city2, {String? city3, String? city4="Chitwan"}) {
+void printCities(String city1, String city2,
+    {String? city3, String? city4 = "Chitwan"}) {
   print(city1);
   print(city2);
   print(city3);
